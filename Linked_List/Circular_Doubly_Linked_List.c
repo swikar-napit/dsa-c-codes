f2deabe44ff4294374baef8483711d84518f197f
swikar-napit <napit.swikar1@gmail.com>
2026-07-29 22:32:35 +0545

    linked list

---
+++ Linked_List/Circular_Doubly_Linked_List.c
@@ -0,0 +1,124 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node{
+    int info;
+    struct Node*next, * previous;
+};
+struct Node*first = NULL;
+struct Node*last = NULL;
+
+void insertfirst(int data){
+    //create a newnode
+    struct Node*newnode = (struct Node*)malloc(sizeof(struct Node));
+    newnode -> info = data;
+    newnode -> next = NULL;
+    newnode -> previous = NULL;
+
+    if(first == NULL){
+        first = last = newnode;
+        return;
+    }
+    else{
+        newnode -> next = first;
+        first -> previous = newnode;
+        first = newnode;
+        last -> next = first;
+        first -> previous = last;
+    }
+}
+
+void insertlast(int data){
+    //create a newnode
+    struct Node*newnode = (struct Node*)malloc(sizeof(struct Node));
+    newnode -> info = data;
+    newnode -> next = NULL;
+
+    if(last == NULL){
+        first = last = newnode;
+        return;
+    }
+    else{
+        last -> next = newnode;
+        newnode -> previous = last;
+        last = newnode;
+        last -> next = first;
+        first -> previous = last;
+    }
+}
+
+void deleteFirst(){
+    if(first == NULL){
+        printf("Enter list");
+        return;
+    }
+    else if(first == last){
+        free(first);
+        first = last = NULL;
+    }
+    else{
+        struct Node*temp = first;
+        first = first -> next;
+        free(temp);
+        first -> previous = NULL;
+        last -> next = first;
+        first -> previous = last;
+    }
+}
+
+void deleteLast(){
+    if(first == NULL){
+        printf("Empty list");
+        return;
+    }
+    else if(first == last){
+        free(last);
+        first = last = NULL;
+    }
+    else{
+        struct Node*temp = last -> previous;
+        while(temp -> next != last){
+            temp = temp -> next;
+        }
+        free(last);
+        last = temp;
+        last -> next = NULL;
+        last -> next = first;
+        first -> previous = last;
+    }
+}
+
+void display(){
+    if(first == NULL){
+        printf("Empty list");
+        return;
+    }
+    else{
+        struct Node*temp = first;
+        while(temp != last){
+            printf("%d ->", temp -> info);
+            temp = temp -> next;
+        }
+        printf("%d ->", temp -> info);
+        printf("\n");
+    }
+}
+
+
+
+int main(){
+    insertfirst(10);
+    insertfirst(5);
+    display();
+
+    insertlast(20);
+    insertlast(25);
+    display();
+
+    deleteFirst();
+    display();
+
+    deleteLast();
+    display();
+    return 0;
+}
